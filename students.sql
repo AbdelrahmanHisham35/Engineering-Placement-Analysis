0f0b67df00d82a68d33f154d1d7b9190bed9391e
AbdelrahmanHisham35 <abdulrahmanhisham35@gmail.com>
2026-03-31 14:47:40 +0200

    Update students.sql

--- students.sql
+++ students.sql
@@ -2,3 +2,4 @@ drop database if exists engineering_analysis;
 
 create database engineering_analysis;
 
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `student_summary_report` AS select `students_clean`.`Branch` AS `Branch`,count(0) AS `total_students`,round(avg(`students_clean`.`Average_GPA`),2) AS `avg_gpa`,round(avg(`students_clean`.`CTC_LPA`),2) AS `avg_salary` from `students_clean` where (`students_clean`.`Placement_Status` = 'Placed') group by `students_clean`.`Branch`;
